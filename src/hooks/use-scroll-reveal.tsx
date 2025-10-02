62fb355368328498c21c1b2f9fb0f4c4097ce532
cerberuuussaf <moises_gomes_@hotmail.com>
2025-10-02 18:41:39 -0300

    aaaa

--- src/hooks/use-scroll-reveal.tsx
+++ src/hooks/use-scroll-reveal.tsx
@@ -1,38 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, MutableRefObject } from "react";
+import scrollReveal from "scrollreveal";
 
-type ScrollRevealOptions = {
-  delay?: number;
-  distance?: string;
-  duration?: number;
-  origin?: 'top' | 'bottom' | 'left' | 'right';
-  easing?: string;
-  scale?: number;
-  reset?: boolean;
-};
-
-export const useScrollReveal = (options: ScrollRevealOptions = {}) => {
-  const elementRef = useRef<HTMLElement>(null);
+/**
+ * Hook personalizado para aplicar animações de scroll a um elemento.
+ * @returns Uma referência React para ser anexada ao elemento do DOM que pretende animar.
+ */
+export function useScrollReveal<T extends HTMLElement>(): [MutableRefObject<T | null>] {
+  const sectionRef = useRef<T>(null);
 
   useEffect(() => {
-    const srPromise = import('scrollreveal');
-    
-    srPromise.then(srModule => {
-      const sr = srModule.default({
-        delay: options.delay || 200,
-        distance: options.distance || '30px',
-        duration: options.duration || 800,
-        origin: options.origin || 'bottom',
-        easing: options.easing || 'ease-in-out',
-        scale: options.scale || 1,
-        reset: options.reset || false,
+    // Verifica se a referência ao elemento existe antes de aplicar o efeito
+    if (sectionRef.current) {
+      // Aplica o efeito de revelação com as opções padrão
+      scrollReveal().reveal(sectionRef.current, {
+        reset: false,
+        delay: 400,
+        origin: 'bottom',
+        distance: '40px'
       });
+    }
+  }, []); // O array vazio garante que o efeito só é aplicado uma vez
 
-      if (elementRef.current) {
-        sr.reveal(elementRef.current);
-      }
-    });
-  }, [options]);
+  return [sectionRef];
+}
 
-  // Type assertion to inform TypeScript about the ref's type
-  return elementRef as React.RefObject<any>;
-};
+
